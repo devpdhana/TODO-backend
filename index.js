ad82f43705303954e63bebc8a539619a6b444970
devpdhana <devpdhansekar@gmail.com>
2023-10-21 10:38:00 +0530

    Backed and frontend updated

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const PORT = 8080
+const mongoose = require('mongoose')
+const cors = require('cors')
+require('./models/db')
+
+const router = require('./routes/routes')
+//middleware
+app.use(express.json())
+app.use(cors());
+app.use('/api/tasks',router)
+
+
+app.listen(PORT,(err)=>{
+    if(err){
+        console.log(err)
+    }else {
+        console.log(`server is running on port ${PORT}`)
+    }
+})
